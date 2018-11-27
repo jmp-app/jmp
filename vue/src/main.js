7feea97f04fd99ae9cee1ed980eedd974bcd74af
Simai00 <simon.friedli@gibmit.ch>
2018-11-27 11:27:22 +0100

    Add semicolons to the template code

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './registerServiceWorker';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function (h) { return h(App); }
+}).$mount('#app');
--- vue/src/router.js
+++ vue/src/router.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -20,8 +20,8 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
+        return import(/* webpackChunkName: "about" */ './views/About.vue');
       }
     }
   ]
-})
+});
