c9179fdcadbb0be8ade493eb16fbd3571b54c69f
Simai00 <simon.friedli@gibmit.ch>
2018-11-27 11:21:43 +0100

    Add Eslint config

--- vue/.eslintrc.js
+++ vue/.eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': [2, 'always'],
+    'indent': [0, 4]
   },
   parserOptions: {
     parser: 'babel-eslint'
