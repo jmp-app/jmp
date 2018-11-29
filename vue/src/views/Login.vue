b66253bebf83c97f5fdd8436ad4b670e89c116c3
Simai00 <simon.friedli@gibmit.ch>
2018-11-29 11:36:50 +0100

    Add form

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <h1>Login</h1>
+        <form>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label" for="username">Benutzername</label>
+                <div class="col-sm-9">
+                    <input class="form-control" id="username" placeholder="Benutzername eingeben" type="text">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label" for="password">Passwort</label>
+                <div class="col-sm-9">
+                    <input class="form-control" id="password" placeholder="Passwort eingeben" type="password">
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Anmelden</button>
+        </form>
     </div>
 </template>
 
