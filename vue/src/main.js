085684cb6286d65ab6b8ed5cee2bd5ec543fc8e9
Simai00 <simon.friedli@gibmit.ch>
2018-11-29 11:47:21 +0100

    Add i18n

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+    i18n,
   render: function (h) { return h(App); }
 }).$mount('#app');
