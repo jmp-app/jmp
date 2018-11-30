d5522ac3502d24c9edbf4037e1001047599288f6
Dominik Strässle <dominik8straessle@gmail.com>
2018-11-30 07:17:15 +0100

    backend-login: Some test routes

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,14 +1,21 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 // API Routes - version 1
-$app->group('/v1', function() {
+$app->group('/v1', function () {
 
     $this->get('/hello', function (Request $request, Response $response, array $args) {
         $response->getBody()->write("Hello World");
         return $response;
     });
+
+
+    $this->get('/login', function (Request $request, Response $response, array $args) {
+        return $response->withJson(['username' => 'bla']);
+    });
+
+    $this->get('/test', \JMP\Controllers\LoginController::class . ':login');
 
 });
