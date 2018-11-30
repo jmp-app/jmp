58683434a9059e4069d299ee9ad3baa994411aee
Simai00 <simon.friedli@gibmit.ch>
2018-11-30 18:38:02 +0100

    Add load on scroll example

--- vue/src/components/EventCard.vue
+++ vue/src/components/EventCard.vue
@@ -1,12 +1,15 @@
 <template>
-    <div :style="{ backgroundColor: event.color, color: getColor() }" class="card mb-3">
+    <!--<div :style="{ backgroundColor: event.color, color: getColor() }" class="card mb-3">-->
+    <div class="card mb-3">
         <div class="card-header">
-            <h5>{{event.title}}</h5>
+            <!--<h5>{{event.title}}</h5>-->
+            <h5>{{event.name.first}}</h5>
         </div>
         <div class="card-body">
             <p class="card-text">
                 {{getDateTime()}} <br>
-                {{event.location}}
+                <!--{{event.location}}-->
+                {{event.location.city}}
             </p>
         </div>
     </div>
@@ -26,8 +29,10 @@
                 return fontColorContrast(this.event.color);
             },
             getDateTime: function () {
-                const from = this.event.from;
-                const to = this.event.to;
+                // const from = this.event.from;
+                const from = this.event.dob.date;
+                // const to = this.event.to;
+                const to = this.event.registered.date;
                 let string = '';
                 if (dateFormat(from, 'shortDate') === dateFormat(to, 'shortDate')) {
                     string = dateFormat(this.event.from, 'dd.mm.yyyy hh:MM "- "');
--- vue/src/views/event/Overview.vue
+++ vue/src/views/event/Overview.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <div :key="event.id" v-for="event in events">
+        <!--<div :key="event.id" v-for="event in events">-->
+        <div :key="event.id.value" v-for="event in events">
             <EventCard :event="event"/>
         </div>
     </div>
@@ -8,6 +9,7 @@
 
 <script>
     import EventCard from '@/components/EventCard';
+    import axios from 'axios';
 
     export default {
         name: 'Overview',
@@ -18,50 +20,34 @@
             };
         },
         methods: {
-            initSample: function () {
-                this.events = [
+            getInitialData: function () {
+                for (var i = 0; i < 5; i++) {
+                    axios.get(`https://randomuser.me/api/`)
+                        .then(response => {
+                            this.events.push(response.data.results[0]);
+                        });
+                }
+            },
+            scroll() {
+                window.onscroll = () => {
+                    let bottomOfWindow =
+                        document.documentElement.scrollTop +
+                        window.innerHeight === document.documentElement.offsetHeight;
 
-                    {
-                        'id': '1',
-                        'title': 'ToArray',
-                        'description': 'An array is a data structure that contains a group of elements. Typically these elements are all of the same data type, such as an integer or string. Arrays are commonly used in computer programs to organize data so that a related set of values can be easily sorted or searched.',
-                        'location': 'Aula Erlimatt',
-                        'color': '#4286f4',
-                        'from': '2018-04-10 19:00:00',
-                        'to': '2018-04-10 21:00:00'
-                    },
-                    {
-                        'id': '2',
-                        'title': 'Coding For Dummies',
-                        'description': 'Coding For Dummies is the perfect location to start! Explore the basics of HTML, CSS, JavaScript, Ruby, and Python and find out how to build great web applications. Once youre up and running, you can continue to refine and practice your skills online',
-                        'location': 'Aula Erlimatt',
-                        'color': '#4286f4',
-                        'from': '2018-04-10 19:00:00',
-                        'to': '2018-04-10 21:00:00'
-                    },
-                    {
-                        'id': '3',
-                        'title': 'Harry porter and the sorcerers stone',
-                        'description': 'For 10 years, Harry lives in the cupboard under the stairs and is subjected to cruel mistreatment by Aunt Petunia, Uncle Vernon and their son Dudley. On his 11th birthday, Harry receives a letter inviting him to study magic at the Hogwarts School of Witchcraft and Wizardry.',
-                        'location': 'Aula Erlimatt',
-                        'color': '#000000',
-                        'from': '2018-04-10 19:00:00',
-                        'to': '2018-04-10 21:00:00'
-                    },
-                    {
-                        'id': '4',
-                        'title': 'Harry porter and the sorcerers stone 2',
-                        'description': 'For 10 years, Harry lives in the cupboard under the stairs and is subjected to cruel mistreatment by Aunt Petunia, Uncle Vernon and their son Dudley. On his 11th birthday, Harry receives a letter inviting him to study magic at the Hogwarts School of Witchcraft and Wizardry.',
-                        'location': 'Aula Erlimatt',
-                        'color': '#ffffff',
-                        'from': '2018-04-10 19:00:00',
-                        'to': '2018-04-11 21:00:00'
+                    if (bottomOfWindow) {
+                        axios.get(`https://randomuser.me/api/`)
+                            .then(response => {
+                                this.events.push(response.data.results[0]);
+                            });
                     }
-                ];
+                };
             }
         },
-        created() {
-            this.initSample();
+        beforeMount() {
+            this.getInitialData();
+        },
+        mounted() {
+            this.scroll();
         }
     };
 </script>
