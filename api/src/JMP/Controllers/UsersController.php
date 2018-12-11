76d36e16f10955cc223d3b421108a861c195ccea
Dominik Strässle <dominik8straessle@gmail.com>
2018-12-11 09:15:27 +0100

    backend-user-actions: Complete the response for createUser

--- api/src/JMP/Controllers/UsersController.php
+++ api/src/JMP/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace JMP\Controllers;
 
 
 use Interop\Container\ContainerInterface;
+use JMP\Models\User;
 use JMP\Services\Auth;
 use JMP\Services\UserService;
 use Slim\Http\Request;
@@ -21,6 +22,10 @@ class UsersController
      * @var UserService
      */
     private $userService;
+    /**
+     * @var callable
+     */
+    private $nullFilter;
 
     /**
      * EventController constructor.
@@ -30,9 +35,10 @@ class UsersController
     {
         $this->auth = $container->get('auth');
         $this->userService = $container->get('userService');
+        $this->nullFilter = $container->get('nullFilter');
     }
 
-    public function createUser(Request $request, Response $response)
+    public function createUser(Request $request, Response $response): Response
     {
         if ($this->auth->requestAdmin($request)->isFailure()) {
             return $response->withStatus(403);
@@ -43,10 +49,22 @@ class UsersController
             return $response->withJson(['errors' => $errors], 400);
         }
 
-        // TODO (dominik): extended validation -> username valid?
+        $user = $request->getParsedBody();
 
+        if ($this->userService->isUsernameUnique($user['username'])) {
+            $user['password'] = password_hash($user['password'], PASSWORD_DEFAULT);
 
-        return $response->withJson($request->getParsedBody());
+            $user = $this->userService->createUser(new User($user));
+
+            return $response->withJson(array_filter((array)$user, $this->nullFilter));
+        } else {
+            return $response->withJson([
+                'errors' => [
+                    'User' => 'A user with the username ' . $user['username'] . ' already exists'
+                ]
+            ], 400);
+        }
     }
+
 
 }
