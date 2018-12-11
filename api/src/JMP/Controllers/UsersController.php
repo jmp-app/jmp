7b058fd677564521a761fa1acfc6137fda2427b1
Dominik Strässle <dominik8straessle@gmail.com>
2018-12-11 18:58:14 +0100

    backend-user-actions: Differentiate between status codes 401 and 403

--- api/src/JMP/Controllers/UsersController.php
+++ api/src/JMP/Controllers/UsersController.php
@@ -42,10 +42,19 @@ class UsersController
      */
     public function createUser(Request $request, Response $response): Response
     {
+        // TODO (dominik): Make a middleware which checks the permissions and creates the error response
+        // Check user for admin permissions
         if ($this->auth->requestAdmin($request)->isFailure()) {
-            return $response->withStatus(403);
+            // No token supplied
+            if ($request->getAttribute('token')) {
+                return $response->withStatus(401);
+            } else {
+                // Token supplied, but no admin permissons
+                return $response->withStatus(403);
+            }
         }
 
+        // TODO (dominik): Make a middleware which checks for errors and creates the error response
         if ($request->getAttribute('has_errors')) {
             $errors = $request->getAttribute('errors');
             return $response->withJson(['errors' => $errors], 400);
@@ -53,19 +62,42 @@ class UsersController
 
         $user = $request->getParsedBody();
 
+        // check if the username is already used by an other user
         if ($this->userService->isUsernameUnique($user['username'])) {
-            $user['password'] = password_hash($user['password'], PASSWORD_DEFAULT);
-
-            $user = $this->userService->createUser(new User($user));
-
-            return $response->withJson(Converter::convert($user));
+            return $this->usernameAvailable($response, $user);
         } else {
-            return $response->withJson([
-                'errors' => [
-                    'User' => 'A user with the username ' . $user['username'] . ' already exists'
-                ]
-            ], 400);
+            return $this->usernameNotAvailable($response, $user);
         }
+    }
+
+    /**
+     * Create the error response if a username is already in use
+     * @param Response $response
+     * @param $user
+     * @return Response
+     */
+    private function usernameNotAvailable(Response $response, $user): Response
+    {
+        return $response->withJson([
+            'errors' => [
+                'User' => 'A user with the username ' . $user['username'] . ' already exists'
+            ]
+        ], 400);
+    }
+
+    /**
+     * Create the response if a user can be created successfully
+     * @param Response $response
+     * @param $user
+     * @return Response
+     */
+    private function usernameAvailable(Response $response, $user): Response
+    {
+        $user['password'] = password_hash($user['password'], PASSWORD_DEFAULT);
+
+        $user = $this->userService->createUser(new User($user));
+
+        return $response->withJson(Converter::convert($user));
     }
 
 
