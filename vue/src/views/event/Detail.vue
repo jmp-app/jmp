a80230dfaf170d0e7109d7923ad85888143e0741
Simai00 <simon.friedli@gibmit.ch>
2018-12-24 18:40:56 +0100

    Add form

--- vue/src/views/event/Detail.vue
+++ vue/src/views/event/Detail.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        <div v-if="event">
-            <EventDetail :event="event"></EventDetail>
-        </div>
         <p v-if="loading">Loading...</p>
         <p v-if="!event && !loading">Not Found</p>
-        <BottomNavigation></BottomNavigation>
+        <div v-if="event">
+            <EventDetail :event="event"></EventDetail>
+            <hr class="mt-4 mb-4"/>
+            <form>
+                <div class="form-group">
+                    <label for="regState">Registration-State</label>
+                    <select class="form-control" id="regState">
+                        <option>Registration state</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="reason">Reason</label>
+                    <input class="form-control" id="reason" placeholder="Enter reason" type="text">
+                </div>
+                <button class="btn btn-danger" type="button">Submit</button>
+            </form>
+            <BottomNavigation></BottomNavigation>
+        </div>
     </div>
 </template>
 
