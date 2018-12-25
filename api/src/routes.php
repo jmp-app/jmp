7521b29fe21f61bfa6e249aeaf8127e2573f27b7
Simai00 <simon.friedli@gibmit.ch>
2018-12-25 12:42:30 +0100

    Backend: Add getAllRegStates

--- api/src/routes.php
+++ api/src/routes.php
@@ -40,6 +40,12 @@ $app->group('/v1', function () {
         ->add(new AuthenticationMiddleware($container, \JMP\Utils\PermissionLevel::USER))
         ->add($jwtMiddleware);
 
+    $this->get('/registration-state', \JMP\Controllers\RegistrationStateController::class . ':getAllRegStates')
+        ->add(new ValidationErrorResponseBuilder())
+        ->add(new Validation($this->getContainer()['validation']['getAllRegStates']))
+        ->add(new AuthenticationMiddleware($container, \JMP\Utils\PermissionLevel::USER))
+        ->add($jwtMiddleware);
+
     $this->post('/users', UsersController::class . ':createUser')
         ->add(new ValidationErrorResponseBuilder())
         ->add(new Validation($this->getContainer()['validation']['createUser']))
