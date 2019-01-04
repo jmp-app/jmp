5cb59fc01cf268474f4e99f357b7cf868ebf7354
mtte <kay.mattern@gibmit.ch>
2019-01-04 16:56:57 +0100

    Init change-password view

---
+++ vue/src/views/ChangePassword.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="card" id="change-password">
+            <form class="card-body">
+                <input type="password" name="new-password" class="form-control" placeholder="New Password">
+                <input type="password" name="new-password-repeat" class="form-control" placeholder="Repeat Password">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ChangePassword'
+    };
+</script>
+
+<style scoped>
+    #change-password {
+        max-width: 35rem;
+    }
+</style>
