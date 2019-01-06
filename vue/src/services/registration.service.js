c96114fe489dad924fcc18b81964fe7b291a1791
Simai00 <simon.friedli@gibmit.ch>
2019-01-06 15:21:00 +0100

    Add updateRegistration

--- vue/src/services/registration.service.js
+++ vue/src/services/registration.service.js
@@ -1,13 +1,24 @@
 import Vue from 'vue';
 
 export const registrationService = {
-    getRegistrationByEventIdAndUserId
+    getRegistrationByEventIdAndUserId,
+    updateRegistration
 };
 
 function getRegistrationByEventIdAndUserId(eventId, userId) {
     return Vue.axios.post(`/registration`, {
         'eventId': eventId,
         'userId': userId
+    })
+        .then(response => {
+            return response.data;
+        });
+}
+
+function updateRegistration(eventId, userId, registrationStateId, reason) {
+    return Vue.axios.put(`/registration/${eventId}/${userId}`, {
+        'registrationState': registrationStateId,
+        'reason': reason
     })
         .then(response => {
             return response.data;
