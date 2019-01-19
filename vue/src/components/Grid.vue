6eecd1a57f73b5f4da5511fdbd409aad4e5a6d3a
Simai00 <simon.friedli@gibmit.ch>
2019-01-19 15:33:43 +0100

    Init new Grid component

---
+++ vue/src/components/Grid.vue
@@ -0,0 +1,100 @@
+<template>
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+        <tr>
+            <th :class="{ active: sortKey === key }"
+                :key="key"
+                @click="sortBy(key)"
+                v-for="key in columns">
+                {{ key | capitalize }}
+                <span :class="sortOrders[key] > 0 ? 'asc' : 'dsc'" class="arrow"></span>
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr :key="entry" v-for="entry in filteredData">
+            <td :key="key" v-for="key in columns">
+                {{entry[key]}}
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: 'Grid',
+        props: {
+            data: Array,
+            columns: Array,
+            filterKey: String
+        },
+        data: function () {
+            let sortOrders = {};
+            this.columns.forEach(function (key) {
+                sortOrders[key] = 1;
+            });
+            return {
+                sortKey: '',
+                sortOrders: sortOrders
+            };
+        },
+        computed: {
+            filteredData: function () {
+                let sortKey = this.sortKey;
+                let filterKey = this.filterKey && this.filterKey.toLowerCase();
+                let order = this.sortOrders[sortKey] || 1;
+                let data = this.data;
+                if (filterKey) {
+                    data = data.filter(function (row) {
+                        return Object.keys(row).some(function (key) {
+                            return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
+                        });
+                    });
+                }
+                if (sortKey) {
+                    data = data.slice().sort(function (a, b) {
+                        a = a[sortKey];
+                        b = b[sortKey];
+                        return (a === b ? 0 : a > b ? 1 : -1) * order;
+                    });
+                }
+                return data;
+            }
+        },
+        filters: {
+            capitalize: function (str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            }
+        },
+        methods: {
+            sortBy: function (key) {
+                this.sortKey = key;
+                this.sortOrders[key] = this.sortOrders[key] * -1;
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .arrow {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0;
+        height: 0;
+        margin-left: 5px;
+        opacity: 0.66;
+    }
+
+    .arrow.asc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-bottom: 4px solid #fff;
+    }
+
+    .arrow.dsc {
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-top: 4px solid #fff;
+    }
+</style>
--- vue/src/views/user/Overview.vue
+++ vue/src/views/user/Overview.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-
+        <form id="search">
+            Search <input name="query" v-model="searchQuery">
+        </form>
+        <grid
+                :columns="gridColumns"
+                :data="gridData"
+                :filter-key="searchQuery">
+        </grid>
     </div>
 </template>
 
 <script>
+    import Grid from '@/components/Grid.vue';
+
     export default {
-        name: 'Overview'
+        name: 'Overview',
+        components: {
+            Grid
+        },
+        data: function () {
+            return {
+                searchQuery: '',
+                gridColumns: ['name', 'power'],
+                gridData: [
+                    {name: 'Chuck Norris', power: Infinity},
+                    {name: 'Bruce Lee', power: 9000},
+                    {name: 'Jackie Chan', power: 7000},
+                    {name: 'Jet Li', power: 8000}
+                ]
+            };
+        }
     };
 </script>
 
