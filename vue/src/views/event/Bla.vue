796071885f9f55244c6b9d014fc169c35e29e3b7
Simon Friedli <simon.friedli@gibmit.ch>
2019-03-05 11:21:06 +0100

    Init new View

---
+++ vue/src/views/event/Bla.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Event Create, Update</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Bla'
+    };
+</script>
+
+<style scoped>
+
+</style>
