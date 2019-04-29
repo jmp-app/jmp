02e0840d9d2938bcc4330207b550e638232d9444
Simon Friedli <simon.friedli@gibmit.ch>
2019-04-29 08:58:40 +0200

    Extract BottomNav to a component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,77 +9,19 @@
                 <div :class="`alert ${alert.type}`" v-if="alert.message">{{alert.message}}</div>
             </v-container>
         </v-content>
-        <v-bottom-nav
-                :value="!isView('login')"
-                app
-                fixed
-        >
-            <v-btn
-                    @click="navigateBack()"
-                    active-class
-                    style="text-decoration: none; color: black !important;"
-            >
-                <span>Back</span>
-                <v-icon>arrow_back</v-icon>
-            </v-btn><!-- Back -->
-            <v-btn
-                    active-class
-                    style="text-decoration: none; color: black !important;"
-                    to="/events/0"
-                    v-if="isView('adminEventOverview')"
-            >
-                <span>{{$t('create')}}</span>
-                <v-icon>add</v-icon>
-            </v-btn><!-- Create Event -->
-            <v-btn
-                    active-class
-                    style="text-decoration: none; color: black !important;"
-                    to="/users/0"
-                    v-if="isView('userOverview')"
-            >
-                <span>{{$t('create')}}</span>
-                <v-icon>add</v-icon>
-            </v-btn><!-- Create User -->
-            <v-btn
-                    active-class
-                    style="text-decoration: none; color: black !important;"
-                    to="/groups/0"
-                    v-if="isView('groupOverview')"
-            >
-                <span>{{$t('create')}}</span>
-                <v-icon>add</v-icon>
-            </v-btn><!-- Create Group -->
-            <v-btn
-                    :to="`/groups/${$route.params.id}/addMembers`"
-                    active-class
-                    style="text-decoration: none; color: black !important;"
-                    v-if="isView('groupDetail')"
-            >
-                <span>{{$t('group.detail.AddMembers')}}</span>
-                <v-icon>build</v-icon>
-            </v-btn><!-- Edit Members from Group -->
-        </v-bottom-nav>
+        <BottomNav></BottomNav>
     </v-app>
 </template>
 
 <script>
     import Navigation from '@/components/Navigation';
+    import BottomNav from '@/components/BottomNav';
 
     export default {
         name: 'app',
         computed: {
             alert() {
                 return this.$store.state.alert;
-            }
-        },
-        methods: {
-            navigateBack: function () {
-                if (window.history.length > 1) {
-                    this.$router.go(-1);
-                }
-            },
-            isView: function (viewName) {
-                return this.$route.name === viewName;
             }
         },
         watch: {
@@ -89,6 +31,7 @@
             }
         },
         components: {
+            BottomNav,
             Navigation
         },
         created() {
---
+++ vue/src/components/BottomNav.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-bottom-nav
+            :value="!isView('login')"
+            app
+            fixed
+    >
+        <v-btn
+                @click="navigateBack()"
+                active-class
+                style="text-decoration: none; color: black !important;"
+        >
+            <span>Back</span>
+            <v-icon>arrow_back</v-icon>
+        </v-btn><!-- Back -->
+        <v-btn
+                active-class
+                style="text-decoration: none; color: black !important;"
+                to="/events/0"
+                v-if="isView('adminEventOverview')"
+        >
+            <span>{{$t('create')}}</span>
+            <v-icon>add</v-icon>
+        </v-btn><!-- Create Event -->
+        <v-btn
+                active-class
+                style="text-decoration: none; color: black !important;"
+                to="/users/0"
+                v-if="isView('userOverview')"
+        >
+            <span>{{$t('create')}}</span>
+            <v-icon>add</v-icon>
+        </v-btn><!-- Create User -->
+        <v-btn
+                active-class
+                style="text-decoration: none; color: black !important;"
+                to="/groups/0"
+                v-if="isView('groupOverview')"
+        >
+            <span>{{$t('create')}}</span>
+            <v-icon>add</v-icon>
+        </v-btn><!-- Create Group -->
+        <v-btn
+                :to="`/groups/${$route.params.id}/addMembers`"
+                active-class
+                style="text-decoration: none; color: black !important;"
+                v-if="isView('groupDetail')"
+        >
+            <span>{{$t('group.detail.AddMembers')}}</span>
+            <v-icon>build</v-icon>
+        </v-btn><!-- Edit Members from Group -->
+    </v-bottom-nav>
+</template>
+
+<script>
+    export default {
+        name: 'BottomNav',
+        methods: {
+            navigateBack: function () {
+                if (window.history.length > 1) {
+                    this.$router.go(-1);
+                }
+            },
+            isView: function (viewName) {
+                return this.$route.name === viewName;
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
